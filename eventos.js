5abe6c2615113105abf7638ec91ae3fd05c9bf46
sabrynnall <limasabrynna03@gmail.com>
2022-05-13 14:22:09 -0300

    criando interface de login

---
+++ eventos.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#form');
+const alerta = document.querySelector('#alerta');
+
+form.onsubmit = (evento) => {
+    evento.preventDefault();
+
+    alerta.style.display = "block";
+
+    setTimeout(() => {
+        alerta.style.display = "none";
+    }, 3000)
+
+}
